60b7d69324d5498a05bc51dcae3861094e9bb6fa
corsa144 <corsa144@gmail.com>
2026-04-28 21:36:57 -0300

    Sobrecargas - ejercicio D01

---
+++ src/main/java/com/mycompany/d01/D01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.d01;
+
+/**
+ *
+ * @author crist
+ */
+public class D01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
